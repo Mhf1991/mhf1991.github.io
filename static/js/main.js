fc08986e361a0ae7e4b7b1b733b21a993443173a
Mhf1991 <mohammad.heydarifard1991@gmail.com>
2025-08-18 12:08:25 +0330

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 let componentsData = [];
 let componentProperties = {};
 
-fetch("/static/comps.csv")
+fetch("/static/comps.json")
   .then((resp) => resp.json())
   .then((data) => {
     componentsData = data;
@@ -282,6 +282,7 @@ function removeSelectedRows() {
 }
 
 updateFractionWarning();
+
 
 
 
